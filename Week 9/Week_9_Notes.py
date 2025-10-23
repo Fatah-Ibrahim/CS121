8c3b2ffe831efa4329e940cdb0946c77063fcbd4
Fatah Ibrahim <ze5141be@go.minnstate.edu>
2025-10-23 09:30:02 -0500

    practice

--- Week 9/Week_9_Notes.py
+++ Week 9/Week_9_Notes.py
@@ -1,16 +1,35 @@
 
 class Planet:
-    def __init__(self, _name):
+    def __init__(self, _name, radius, mass, distance):
         self.name = _name
+        self.radius = radius
+        self.mass = mass
+        self.distance = distance
     
     def get_name(self):
         return self.name
+    
+    def get_radius(self):
+
+        # radius formula
+
+        return self.radius
+    
+    def get_mass(self):
+        return self.mas
+    
+    def get_distance(self):
+        return self.distance
+    
+    
 
 
 Planet_1 = Planet('x25')
 Planet_2 = Planet('')
 
 print(Planet_1.get_name())
+
+
 
 
 # class Dog:
