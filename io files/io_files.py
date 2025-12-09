aed75781fd2dd43761fa2969771ed0eac5c7a779
Fatah Ibrahim <ze5141be@go.minnstate.edu>
2025-12-09 10:02:12 -0600

    done

--- io files/io_files.py
+++ io files/io_files.py
@@ -41,6 +41,7 @@ import random
 #             if i % 2 == 0:
 #                 d.write(line)
 
+enumerate
 #3
 
 # with open('MyName.txt', 'w') as f:
@@ -61,33 +62,94 @@ import random
     
 
 #6 
-with open('aMorePerfectUnion.txt', 'r') as f:
-    amount_of_letters = {}
-    file_context = f.read()
-    lower = file_context.lower()
-    for letter in lower:
-        if letter in amount_of_letters:
-            amount_of_letters[letter] += 1
-        else:
-            amount_of_letters[letter] = 1
+# with open('aMorePerfectUnion.txt', 'r') as f:
+#     amount_of_letters = {}
+#     file_context = f.read()
+#     lower = file_context.lower()
+#     for letter in lower:
+#         if letter in amount_of_letters:
+#             amount_of_letters[letter] += 1
+#         else:
+#             amount_of_letters[letter] = 1
 
-print(amount_of_letters)
+# print(amount_of_letters)
+
+#8
+
+# with open('CaloriesBurnedData.txt', 'r') as f:
+#     max = 0
+#     best_date = None
+#     next(f)
+#     for line in f:
+#         n = line.split()
+#         date = n[0]
+#         amount = int(n[1])
+
+#         if amount > max :
+#             max = amount
+#             best_date = date 
+    # print(best_date, max)
+
+#10
+
+with open('PagesRead.csv', 'r') as f:
+    pages_read = {}
+    next(f)
+    for line in f:
+        track = line.split(',')
+        name = track[0]
+        total_pages = int(track[1]) + int(track[2])
+        if name in pages_read:
+            pages_read[name] += total_pages
+        else:
+            pages_read[name] = total_pages
+
+    print(pages_read)
+
+
 
 
 #11
 
 # with open('SongPlays.txt', 'r') as f:
+#     plays = {}
 #     next(f)
-#     amount_plays = {}
 #     for line in f:
-#         parts = line.strip().split()
-#         if len(parts) >= 2:
-#             name = parts[0]
-#             play_count = int(parts[1])
-#             amount_plays[name] = play_count
-#     print(amount_plays)
+#         parts = line.split()
+#         name = parts[0]
+#         listens = int(parts[1])
+#         if name in plays:
+#             plays[name] += listens
+#         else:
+#             plays[name] = listens
+#     print(plays)
+
 
 #12
+
+# with open('DailyTemperatures.csv', 'r') as f:
+#     max = 0
+#     min = 99999
+#     total = 0
+#     count = 0
+#     next(f)
+#     for line in f:
+#         parts = line.split(',')
+#         n = int(parts[1])
+
+#         if n > max:
+#             max = n
+
+#         if n < min:
+#             min = n
+
+#         total += n
+
+#         count += 1
+
+#         average = total/count
+#     print(max, min, average)
+
 
 
         
